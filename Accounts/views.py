e6d528cf0e9291d2b27395819bf68018b8b634bf
dreytted <victorkiariem@gmail.com>
2024-02-13 22:55:26 +0300

    registration view

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView, Response, status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 # Create your views here.
 
@@ -24,3 +25,40 @@ class LoginView(APIView):
                             'refresh_token': str(refresh)}, status=status.HTTP_200_OK)
         except serializers.ValidationError as e:
             return Response({"errors": str(e)})
+
+
+class RegistrationView(APIView):
+    throttle_classes = [AnonRateThrottle]
+
+    def post(self, request):
+
+        serializer = PendingDataSerializer(data=request.data)
+        if serializer.is_valid():
+            with transaction.atomic():
+                try:
+                    # get all the data from the request
+                    email = serializer.validated_data['email']
+                    username = serializer.validated_data['username']
+                    phone_number = serializer.validated_data['phone_number']
+                    password = serializer.validated_data['password']
+
+                    # handle otp generation and sending
+                    # otp_code = otp_generator.otp_generator()
+
+                    otp_instance = Otp.objects.create(otp_code=otp_code)
+                    otp_instance.save()
+                    # response = email_sender.sendEmail(
+                        # username=username, otp_code=otp_code, email=email)
+                    if response:
+                        # handling save PendingUserData
+                        pending_user = PendingUserModel.objects.create(
+                            user_otp=otp_code, username=username, email=email, phone_number=phone_number, password=password, date_of_birth=date_of_birth)
+                        pending_user.save()
+                        return Response({'Detail': 'Otp code sent successfully'}, status=status.HTTP_200_OK)
+
+                    raise Exception('Error encountered when sending Email')
+
+                except Exception as e:
+                    return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
