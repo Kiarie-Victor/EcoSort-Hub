3e8a1b95bf269658464157220b497ae880d6581f
dreytted <victorkiariem@gmail.com>
2024-04-04 22:10:26 +0300

    regsitered models in admin

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Accounts.models import Otp, PendingUserModel, Member
+
 
 # Register your models here.
+admin.site.register(Otp)
+admin.site.register(PendingUserModel)
+admin.site.register(Member)
--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import EnvironmentalTipModel, WasteCategory
 
 # Register your models here.
+
+admin.site.register(EnvironmentalTipModel)
+admin.site.register(WasteCategory)
