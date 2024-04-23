de9b0c7b3cdc8c5b054942db8c7a85bf7db39d9a
dreytted <victorkiariem@gmail.com>
2024-04-23 15:46:49 +0300

    environmental tip url

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('funFact', views.DidYouKnowView.as_view())
-    path('waste-category', views.WasteCategoryView.as_view())
+    # path('funFact', views.DidYouKnowView.as_view()),
+    path('waste-category', views.WasteCategoryView.as_view()),
+    path('environmental_tip', views.EnvironmentalTipView.as_view())
 ]
